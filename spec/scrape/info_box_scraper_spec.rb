40f12f4c4b4f50b845b9ad6653bbb56a8fa77ea2
Andre Menezes <djdedemenezes@gmail.com>
2022-05-27 11:34:49 -0300

    initialize test for info box scraper

---
+++ spec/scrape/info_box_scraper_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe InfoBoxScraper, type: :service do
+  describe '#initialize' do
+    path = '/wiki/Harry_Potter'
+    doc = Nokogiri::HTML(Faraday.get(path).body)
+    info_box_scraper = InfoBoxScraper.new doc: doc
+    it 'with an empty informations hash' do
+      actual = info_box_scraper.informations
+      expect(actual).to be_a(Hash)
+      expect(actual.size).to eq(0)
+    end
+  end
+end
