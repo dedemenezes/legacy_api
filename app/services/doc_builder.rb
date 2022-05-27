44133142e393f21e8a61daa149be528b39a9574b
Andre Menezes <djdedemenezes@gmail.com>
2022-05-27 13:56:47 -0300

    rename url to path in docbuild initialize

--- app/services/doc_builder.rb
+++ app/services/doc_builder.rb
@@ -3,14 +3,14 @@ class DocBuilder
   attr_reader :html_doc
 
   def initialize(attributes = {})
-    @url = attributes[:url]
+    @path = attributes[:path]
     @file_path = attributes[:file_path]
-    build_nokogiri_doc_from_url if @url
+    build_nokogiri_doc_from_url if @path
     build_nokogiri_doc_from_file if @file_path
   end
 
   def build_nokogiri_doc_from_url
-    html_file = Faraday.get("#{BASE_URL}#{@url}").body
+    html_file = Faraday.get("#{BASE_URL}#{@path}").body
     @html_doc = Nokogiri::HTML(html_file)
     @html_doc
   end
