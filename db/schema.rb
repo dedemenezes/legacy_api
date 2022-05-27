4ee9c726de5efe78a030dc1a3281b842e40bd103
Andre Menezes <djdedemenezes@gmail.com>
2022-05-27 16:48:49 -0300

    updated to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_05_27_182152) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "path"
