ab9a7fa878c96e54734723a70e66fce9526c3390
Andre Menezes <djdedemenezes@gmail.com>
2022-05-27 16:30:52 -0300

    no content when db is empty

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::BooksController < ApplicationController
   def index
     @books = Book.all
-    binding.pry
+    return render json: { message: 'Still no books to display' }, status: 204 if @books.empty?
+
     render json: @books.to_json, status: 200
   end
 end
--- spec/requests/api/v1/books_spec.rb
+++ spec/requests/api/v1/books_spec.rb
@@ -3,20 +3,47 @@ require 'rails_helper'
 RSpec.describe "Api::V1::Books", type: :request do
   describe "GET #index" do
 
-    before do
-      create(:book)
-      get '/api/v1/books'
+
+    context "when database is empty" do
+      it 'returns http status no content when no book in the db' do
+        get '/api/v1/books'
+        expect(response).to have_http_status(:no_content)
+      end
     end
 
-    it "returns http success" do
+    context 'when datbase has records' do
+      before do
+        create(:book)
+        get '/api/v1/books'
+      end
+
+      it "returns http success" do
+        expect(response).to have_http_status(:success)
+      end
+
+      it "JSON body response contains all books attributes" do
+        json_response = JSON.parse(response.body)
+        expect(json_response).to be_an(Array)
+        keys = json_response.first.keys
+        expect(keys).to include("title").and include("image_url").and include("author")
+      end
+    end
+  end
+
+  describe "GET #show" do
+    before do
+      create(:book)
+      get '/api/v1/books/1'
+    end
+
+    it 'returns http success' do
       expect(response).to have_http_status(:success)
     end
 
-    it "JSON body response contains all books attributes" do
+    it 'JSON body response contains expected book attributes' do
       json_response = JSON.parse(response.body)
-      expect(json_response).to be_an(Array)
-      keys = json_response.first.keys
-      expect(keys).to include("title").and include("image_url").and include("author")
+      expect(json_response).to be_a(Hash)
+      expect(json_response.keys).to include("title").and include("image_url").and include("author")
     end
   end
 end
