ee0498a4678e30c35b2db8ec8ffa262069c55321
Andre Menezes <djdedemenezes@gmail.com>
2022-05-27 14:42:02 -0300

    updated table columns to match hash built

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_27_171518) do
+ActiveRecord::Schema.define(version: 2022_05_27_173145) do
 
   create_table "books", force: :cascade do |t|
-    t.string "info"
-    t.string "url"
+    t.string "title"
+    t.string "path"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
