e92c571cdd95da7a0cd79e9d65c94dba69152678
Andre Menezes <djdedemenezes@gmail.com>
2022-05-28 01:37:44 -0300

    scraping character index url from book page

---
+++ app/services/character_index_url_scraper.rb
@@ -0,0 +1,14 @@
+class CharacterIndexUrlScraper
+  def initialize(attributes = {})
+    @doc = attributes[:doc]
+  end
+
+  def scrape
+    index_url = ''
+    ul = @doc.search('span#See_also.mw-headline').first.parent.next.next
+    ul.search('a').each do |li|
+      index_url = li.attr('href') if li.attr('title').match? /index/
+    end
+    index_url
+  end
+end
---
+++ spec/scrape/character_index_url_scraper_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+# require_relative '../../app/services/doc_builder'
+
+RSpec.describe CharacterIndexUrlScraper, type: :service do
+  describe '#scrape' do
+    it 'returns correct url' do
+      create :book
+      doc = DocBuilder.new(path: Book.first.path).html_doc
+      cius = CharacterIndexUrlScraper.new doc: doc
+      actual = cius.scrape
+      expect(actual).to eq('/wiki/Harry_Potter_and_the_Philosopher%27s_Stone_(character_index)')
+    end
+  end
+end
