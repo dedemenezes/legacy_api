3a6d2db1d02b8a8c32abb6a6fdcc188da1d32528
Andre Menezes <djdedemenezes@gmail.com>
2022-06-10 20:01:31 -0300

    added custom association methods

--- app/models/wand.rb
+++ app/models/wand.rb
@@ -3,6 +3,8 @@
 class Wand < ApplicationRecord
   has_one :wand_master, dependent: :destroy
   has_many :wand_owners, dependent: :destroy
+  has_one :master, through: :wand_master, source: :character
+  has_many :owners, through: :wand_owners, source: :character
 
   def self.generate_attribute_hash(infos)
     hsh = infos.map do |key, values|
@@ -18,13 +20,5 @@ class Wand < ApplicationRecord
 
   def self.has_association?(attribute)
     %i[owners masters masters_url].include? attribute
-  end
-
-  def master
-    wand_masters.character
-  end
-
-  def owners
-    wand_owners.map(&:character)
   end
 end
