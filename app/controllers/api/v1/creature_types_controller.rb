235e043d7038bc0040b33a94bd0ee9c360111da8
Andre Menezes <djdedemenezes@gmail.com>
2022-06-10 15:50:48 -0300

    index and show for creature types done

---
+++ app/controllers/api/v1/creature_types_controller.rb
@@ -0,0 +1,18 @@
+module Api
+  module V1
+    class CreatureTypesController < ActionController::Base
+      append_view_path "#{Rails.root}/app/views"
+
+      def index
+        @creature_types = CreatureType.all
+        render json: @creature_types.to_json, status: 200
+      end
+
+      def show
+        @creature_type = CreatureType.find(params[:id])
+
+        render json: @creature_type.to_json, status: 200
+      end
+    end
+  end
+end
