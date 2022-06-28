358f1a401b2f805e62e3126b3098214aad4cda43
Andre Menezes <djdedemenezes@gmail.com>
2022-06-27 22:46:37 -0300

    renamed to match class name

--- spec/services/seeds/build_creature_type_spec.rb
+++ spec/services/seeds/build_creature_type_spec.rb
@@ -6,9 +6,9 @@ RSpec.describe Seeds::CreatureTypes::BuildCreatureType, type: :service do
   VCR.use_cassette('human') do
     describe '#from_path' do
       it 'create Human' do
+        expect(CreatureType.count).to eq(0)
         subject = described_class.new(path: '/wiki/Human')
         subject.from_path
-        binding.pry
         expect(subject.creature_type).to eq(CreatureType.last)
       end
     end
