2c9800d294b1cf2cffe9040e47d5100f24e6ef87
Andre Menezes <djdedemenezes@gmail.com>
2022-06-27 23:10:29 -0300

    simple simple controller test

--- spec/requests/api/v1/houses_spec.rb
+++ spec/requests/api/v1/houses_spec.rb
@@ -4,6 +4,17 @@ require 'rails_helper'
 
 RSpec.describe 'Api::V1::Houses', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'returns http success' do
+      get '/api/v1/houses'
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET /show' do
+    it 'returns http success' do
+      create(:gryffindor)
+      get "/api/v1/houses/#{House.first.name}"
+      expect(response).to have_http_status(:success)
+    end
   end
 end
