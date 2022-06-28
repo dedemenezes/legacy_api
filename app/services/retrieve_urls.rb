fd25da951ba9c4421e9970d35c4c8b130aecbafe
Andre Menezes <djdedemenezes@gmail.com>
2022-06-28 20:31:53 -0300

    removing wrong urls and nil values

--- app/services/retrieve_urls.rb
+++ app/services/retrieve_urls.rb
@@ -2,7 +2,14 @@
 
 module RetrieveUrls
   def self.script(klass, attribute_url)
-    klass.to_s.downcase.capitalize.constantize.pluck(attribute_url).uniq.compact
+    klass.to_s
+         .downcase
+         .capitalize
+         .constantize
+         .pluck(attribute_url)
+         .compact
+         .reject { |url| url.include? '#' }
+         .uniq
     # Character.pluck(:species_url).uniq.compact
   end
 end
