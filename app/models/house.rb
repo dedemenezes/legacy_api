cf364c79c46ada720c690462956a7b10a8554864
Andre Menezes <djdedemenezes@gmail.com>
2022-06-28 23:29:08 -0300

    optmized books controller queries

--- app/models/house.rb
+++ app/models/house.rb
@@ -5,14 +5,23 @@ class House < ApplicationRecord
   validates :name, :path, presence: true
   validates :name, uniqueness: { scope: :path }
   has_many :members, dependent: :destroy
+  has_many :characters, through: :members
   has_many :heads, dependent: :destroy
+  has_many :headers, through: :heads, source: :header
   has_many :distinctions, as: :record, dependent: :destroy
 
   def traits_to_s
-    return '' if distinctions.empty?
 
-    last = distinctions[-1].content
-    start = distinctions[0...-1].map(&:content)
-    distinctions.count > 1 ? "#{start.join(', ')} and #{last}" : distinctions.first.content
+    case distinctions.size
+    when 0 then ''
+    when 1 then distinctions.first.content
+    else
+      copy = distinctions.to_a
+      last = copy.pop
+      copy.join(', ').concat(" and #{last}")
+    end
+    # last = distinctions[-1].content
+    # start = distinctions[0...-1].map(&:content)
+    # distinctions.count > 1 ? "#{start.join(', ')} and #{last}" : distinctions.first.content
   end
 end
--- app/views/api/v1/houses/show.json.jbuilder
+++ app/views/api/v1/houses/show.json.jbuilder
@@ -2,9 +2,18 @@
 
 json.name @house.name
 json.founder @house.founder
-json.image_url @house.image_url
 json.animal @house.animal
+json.traits @house.traits_to_s
 json.element @house.element
+json.colors @house.colors
+json.image_url @house.image_url
 json.ghost @house.ghost
 json.common_room @house.common_room
-json.members @house.members.count
+json.heads @house.heads do |head|
+  json.id head.header.id
+  json.name head.header.name
+end
+json.members @house.members do |member|
+  json.id member.character.id
+  json.name member.character.name
+end
