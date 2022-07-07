f11aae75f84556bc72210f91292e28f8e57c2f44
Andre Menezes <djdedemenezes@gmail.com>
2022-07-07 13:37:24 -0300

    fixed test

--- spec/requests/api/v1/pages_spec.rb
+++ spec/requests/api/v1/pages_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Pages", type: :request do
-  describe "GET /main" do
+  describe "GET /" do
     it "returns http success" do
-      get "/api/v1/pages/main"
+      get "/api/v1/"
       expect(response).to have_http_status(:success)
     end
   end
