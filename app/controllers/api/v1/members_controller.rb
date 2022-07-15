8faab76e8aa21ccbffdb5987c7c74ee0cff11856
Andre Menezes <djdedemenezes@gmail.com>
2022-07-15 10:44:09 -0300

    updated member to use house id

--- app/controllers/api/v1/members_controller.rb
+++ app/controllers/api/v1/members_controller.rb
@@ -4,7 +4,7 @@ module Api
   module V1
     class MembersController < ApplicationController
       def index
-        @house = House.find_by_name(params[:house_name].capitalize)
+        @house = House.find_by_name(params[:house_id].capitalize)
         @members = Member.includes(:character).where(house: @house)
       end
     end
